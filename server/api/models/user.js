acdb4c19deaaaf60dfb663d8277d2888388bd1b2
vmurali100 <vmurali100@gmail.com>
2019-05-21 18:07:29 +0530

    Adding Clint Changes

--- server/api/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  email: String,
-  password: String
-});
-
-module.exports = mongoose.model("User", userSchema);
